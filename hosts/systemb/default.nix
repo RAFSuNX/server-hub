3433a3acb3e13e7c9f131ecdfa58aae5c76ec657
RAFSuNX <ashek.hasan@aiavatar.work>
2026-06-06 13:57:12 +0600

    security: add hermes key to systemb/systemc, harden kernel sysctl, disable rpcbind

--- hosts/systemb/default.nix
+++ hosts/systemb/default.nix
@@ -13,5 +13,6 @@
 
   users.users.rafsunx.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMXg4tKSm2U39RkTY1tT/q3Mk8ijwSHbBySXH7+sY5wT rafsunx@systemb"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJr9/jtmLdc7hWjNyWXwG1DA1ZgSR/4WRpE/cOn1a2uI hermes"
   ];
 }
