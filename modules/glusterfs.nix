abf0e4cf35d7b006d6862f6c277d13d005aa36b3
RAFSuNX <git@rafsunx.com>
2026-09-05 01:58:30 +0600

    fix(nixos): use full paths for mount/mountpoint in glusterfs-mount script

--- modules/glusterfs.nix
+++ modules/glusterfs.nix
@@ -35,8 +35,8 @@
       RemainAfterExit = true;
       ExecStart       = pkgs.writeShellScript "glusterfs-mount" ''
         for i in $(seq 1 24); do
-          mountpoint -q /mnt/storage && exit 0
-          mount /mnt/storage && exit 0
+          ${pkgs.util-linux}/bin/mountpoint -q /mnt/storage && exit 0
+          ${pkgs.util-linux}/bin/mount /mnt/storage && exit 0
           sleep 5
         done
         exit 1
